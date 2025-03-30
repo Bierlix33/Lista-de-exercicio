eeee8621fb0075ccd981ee51028f3de782a4a528
Bierlix33 <Allangab21@gmail.com>
2025-03-30 09:48:44 -0300

    Create metros_para_centimetros.c

---
+++ metros_para_centimetros.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+  float m;
+  printf("Digite o valor em metros: ");
+  scanf("%f", &m);
+  printf("conversão de metro para centimetros é: %.2f",m*100);
+  
+  return 0;
+}
